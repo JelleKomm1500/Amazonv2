5c98f81b0553dfec3b643272850d1d93769dccc5
JelleKomm1500 <32571630+JelleKomm1500@users.noreply.github.com>
2018-11-19 14:01:50 +0100

    Music + Start button

---
+++ AmazonSimulator VS/AmazonSimulator VS/wwwroot/js/Music.js
@@ -0,0 +1,45 @@
+﻿var audio = new Audio("music/Stardew Valley OST - Summer (Natures Crescendo).mp3");
+
+function startMusic() {
+    var x = document.getElementById("start");
+    console.log(x);
+    var y = document.getElementById("mute");
+    console.log(y);
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+    audio.play();
+}
+
+function playMusic() {
+    var x = document.getElementById("play");
+    var y = document.getElementById("mute");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+
+    audio.play();
+
+}
+
+function stopMusic() {
+    var x = document.getElementById("play");
+    var y = document.getElementById("mute");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+        x.style.display = "block";
+    } else {
+        y.style.display = "block";
+        x.style.display = "none";
+    }
+
+    audio.pause();
+}
+
